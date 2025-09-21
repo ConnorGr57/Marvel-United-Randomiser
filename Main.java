e7fc7e71482dbece7c0635a6b863dc26425a8b42
ConnorGr57 <connorgr57@gmail.com>
2025-09-21 11:03:38 +0100

    Update Main.java
    updated main to add hero class

--- Main.java
+++ Main.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+import static java.lang.Character.toUpperCase;
+
 public class Main {
     public static void main(String[] args) {
-            Villains villains = new Villains();
-            villains.Villains();
+        Scanner scanner = new Scanner(System.in);
+        char chalOrVil;
+        boolean valid = false;
+        while (!valid) {
+            System.out.println("---------------------------------------------------------------------");
+            System.out.println("Would You Like To Play Against A Villain(v) Or A Challenge(c)");
+            System.out.println("---------------------------------------------------------------------");
+            chalOrVil = scanner.nextLine().charAt(0);
+            chalOrVil = toUpperCase(chalOrVil);
+            if (chalOrVil == 'V') {
+                Villains villains = new Villains();
+                valid = true;
+            } else if (chalOrVil == 'C') {
+                Challenge challenge = new Challenge();
+                valid = true;
+            }
+            else{
+                System.out.println("Invalid Input: Try Again");
+            }
         }
+        Heroes heroes = new Heroes();
+    }
     }
